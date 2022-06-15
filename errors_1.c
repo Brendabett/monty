111f1c60f765c25561dbd4348c50e1fc83741391
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-06-15 18:16:42 +0300

    My commit

--- errors_1.c
+++ errors_1.c
@@ -36,7 +36,7 @@ return (EXIT_FAILURE);
  */
 int f_open_error(char *filename)
 {
-fprintf(stderr, "Error: Cant open file %sn, filename);
+fprintf(stderr, "Error: Cant open file %s\n", filename);
 return (EXIT_FAILURE);
 }
 
@@ -49,7 +49,7 @@ return (EXIT_FAILURE);
  */
 int unknown_op_error(char *opcode, unsigned int line_number)
 {
-fprintf(stderr, L%u: unknown instruction %sn,
+fprintf(stderr, "L%u: unknown instruction %s\n",
 line_number, opcode);
 return (EXIT_FAILURE);
 }
@@ -62,9 +62,6 @@ return (EXIT_FAILURE);
  */
 int no_int_error(unsigned int line_number)
 {
-fprintf(stderr, L%u: usage: push integern, line_number);
+fprintf(stderr, "L%u: usage: push integer\n", line_number);
 return (EXIT_FAILURE);
-}' > errors_1.c
-'errors_1.c
-' > errors_1.c
-
+}
